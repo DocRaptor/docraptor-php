da5d66aeecb7515dc663ac5b5accde7e26d27d80
David Ronk <david@expectedbehavior.com>
2020-07-30 14:32:52 -0400

    Set php agent to 2.0.0

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -88,7 +88,7 @@ class Configuration
      *
      * @var string
      */
-    protected $userAgent = 'Swagger-Codegen/1.3.0/php';
+    protected $userAgent = 'Swagger-Codegen/2.0.0/php';
 
     /**
      * Debug switch (default set to false)
@@ -397,7 +397,7 @@ class Configuration
         $report .= '    OS: ' . php_uname() . PHP_EOL;
         $report .= '    PHP Version: ' . PHP_VERSION . PHP_EOL;
         $report .= '    OpenAPI Spec Version: 2.0.0' . PHP_EOL;
-        $report .= '    SDK Package Version: 1.3.0' . PHP_EOL;
+        $report .= '    SDK Package Version: 2.0.0' . PHP_EOL;
         $report .= '    Temp Folder Path: ' . self::getDefaultConfiguration()->getTempFolderPath() . PHP_EOL;
 
         return $report;
