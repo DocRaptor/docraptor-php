1f1c47c404e3ecda3c8183897e66387cad08b8e9
James Paden <james@jamespaden.com>
2022-09-09 18:17:51 -0400

    Update with latest code generator files

---
+++ examples/hosted_sync.php
@@ -0,0 +1,47 @@
+<?php
+# As a paid add-on, DocRaptor can provide long-term, publicly-accessible hosting for your documents.
+# This allows you to provide a URL to your end users, third party tools like Zapier and Salesforce,
+# or anyone else. We'll host the document on your behalf at a completely unbranded URL for as long
+# as you want, or within the limits you specify.
+#
+# This example demonstrates creating a PDF that DocRaptor will host for you using common options.
+# By default, hosted documents do not have limits on downloads or hosting time, though you may
+# pass additional parameters to the document generation call to set your own limits. Learn more
+# about the specific options in the hosted API documentation.
+# https://docraptor.com/documentation/api#api_hosted
+#
+# It is created synchronously, which means DocRaptor will allow it to generate for up to 60 seconds.
+# Since this document will be hosted by DocRaptor the response from this request will return a JSON
+# formatted object containing public URL where the document can be downloaded from.
+#
+# DocRaptor supports many CSS and API options for output customization. Visit
+# https://docraptor.com/documentation/ for full details.
+#
+# You can run this example with: php hosted_sync.php
+
+$docraptor = new DocRaptor\DocApi();
+# this key works in test mode!
+$docraptor->getConfig()->setUsername("YOUR_API_KEY_HERE");
+
+try {
+    $doc = new DocRaptor\Doc();
+    $doc->setTest(true); # test documents are free but watermarked
+    $doc->setDocumentType("pdf");
+    $doc->setDocumentContent("<html><body>Hello World!</body></html>");
+    # $doc->setDocumentUrl("https://docraptor.com/examples/invoice.html");
+    # $doc->setJavascript(true);
+    # $prince_options = new DocRaptor\PrinceOptions();
+    # $doc->setPrinceOptions($prince_options);
+    # $prince_options->setMedia("print"); # @media 'screen' or 'print' CSS
+    # $prince_options->setBaseurl("https://yoursite.com"); # the base URL for any relative URLs
+
+    # different method than the non-hosted documents
+    $response = $docraptor->createHostedDoc($doc);
+
+    echo "The PDF is hosted at " . $response->getDownloadUrl() . "\n";
+} catch (DocRaptor\ApiException $error) {
+    echo $error . "\n";
+    echo $error->getMessage() . "\n";
+    echo $error->getCode() . "\n";
+    echo $error->getResponseBody() . "\n";
+}
